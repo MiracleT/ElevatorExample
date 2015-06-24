d5d5fbceb1da0c495f97ba4c93bcd6a5d4b79a7f
MiracleT <talitadsouza130@gmail.com>
2015-06-24 15:44:34 +0530

    version 1

---
+++ assign1/src/elevator/EleMain.java
@@ -0,0 +1,11 @@
+package elevator;
+
+public class EleMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//Elevator example to practice GIT tools
+		
+	}
+
+}
