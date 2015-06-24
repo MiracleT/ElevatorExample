0849e23bd48269a12c1faba04a4e18c80f6c44b8
Melroy <melroy.fernz@gmail.com>
2015-06-24 16:18:11 +0530

    added functions to Button and Main, Added new Class Controller

---
+++ assign1/src/elevator/Controller.java
@@ -0,0 +1,21 @@
+package elevator;
+
+public class Controller
+{
+	ElevatorButton button = new ElevatorButton();
+	ElevatorDoor door = new ElevatorDoor();
+	
+	public void Open()
+	{
+		if(!door.getIsOpen())
+		{
+			door.setIsOpen(true);
+			door.openDoor();
+		}
+		else
+		{
+			door.setIsOpen(false);
+			door.closeDoor();
+		}
+	}
+}
