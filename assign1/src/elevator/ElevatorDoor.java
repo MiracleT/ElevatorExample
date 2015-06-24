294bf23958ddfbd5bdea1ceba35807324dbf6f72
MiracleT <talitadsouza130@gmail.com>
2015-06-24 16:05:46 +0530

    added panel and door class functions

--- assign1/src/elevator/ElevatorDoor.java
+++ assign1/src/elevator/ElevatorDoor.java
@@ -2,4 +2,10 @@ package elevator;
 
 public class ElevatorDoor {
 
+	public void openDoor(){
+		System.out.println("open the door");
+	}
+	public void closeDoor(){
+		System.out.println("close the door");
+	}
 }
--- assign1/src/elevator/ElevatorPanel.java
+++ assign1/src/elevator/ElevatorPanel.java
@@ -1,5 +1,19 @@
 package elevator;
 
 public class ElevatorPanel {
-
+	public void openDoorIndicator(){
+		System.out.println("Door is open");
+	}
+	public void closeDoor(){
+		System.out.println("Door is closed");
+	}
+	public void up(){
+		System.out.println("Elevator moving up");
+	}
+	public void down(){
+		System.out.println("Elevator moving down");
+	}
+	public void floor(){
+		System.out.println("FloorNumber 5");
+	}
 }
