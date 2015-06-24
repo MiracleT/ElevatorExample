86694ec718ad2ecb009c5be37b5503ad8e8e3c86
MiracleT <talitadsouza130@gmail.com>
2015-06-24 17:11:25 +0530

    added display panel function to controller class

--- assign1/src/elevator/Controller.java
+++ assign1/src/elevator/Controller.java
@@ -4,6 +4,7 @@ public class Controller
 {
 	ElevatorButton level1 = new ElevatorButton();
 	ElevatorButton level2 = new ElevatorButton();
+	ElevatorPanel panel = new ElevatorPanel();
 	ElevatorDoor door = new ElevatorDoor();
 	
 	public void Open()
@@ -23,7 +24,13 @@ public class Controller
 	public void changeLevel()
 	{
 		System.out.println("change level");
-		
 			
+	}
+	
+	public void displayPanel(){
+		panel.closeDoor();
+		panel.openDoorIndicator();
+		panel.up();
+		panel.down();
 	}
 }
--- assign1/src/elevator/EleMain.java
+++ assign1/src/elevator/EleMain.java
@@ -6,9 +6,11 @@ public class EleMain
 	public static void main(String[] args) 
 	{
 		Controller control = new Controller();
+		control.displayPanel();
 		control.Open();
 		control.changeLevel();
 		control.Open();
+		
 	}
 
 }
